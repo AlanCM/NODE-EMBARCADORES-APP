71d6311406461b916cebdea177af615a80384730
Alan Cardel <alan.cardelm@gmail.com>
2018-10-01 23:07:37 -0700

    fixing get method

--- app/controllers/temporadas.js
+++ app/controllers/temporadas.js
@@ -1,6 +1,6 @@
 var Temporada = require('../models/temporada');
  // import temporada model
-exports.getTemporada = function(req, res, next){
+exports.getTemporadas = function(req, res, next){
     Temporada.find(function(err, temporadas) {
         if (err){
          res.send(err);
