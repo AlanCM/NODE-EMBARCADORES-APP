5de910a20386e7d4d99b9697073990720b11120b
Alan Cardel <alan.cardelm@gmail.com>
2018-10-10 07:37:34 -0700

    productoScheduler agregado

---
+++ app/controllers/productoScheduler.js
@@ -0,0 +1,34 @@
+var http = ('http');
+var MongoClient = require('mongodb').MongoClient;
+// Base de datos
+var url = process.env.MONGODB_URI;
+var jsonStr = '';
+var jProductos;
+console.log('productos sync');
+var fecha = 'FechaModificacion=20180316';
+var options = {
+    host: 'http://appembarcadores.tripleh.com.mx/Default.aspx',
+    path: 'api/Embarques?Catalogo=Producto' + fecha
+};
+
+callback = function(response){
+    response.on('data',function(chunk){
+        jsonStr += chunk;
+    });
+
+    response.on('end',function(){
+        jProductos = JSON.parse(jsonStr);
+        jsonStr = '';
+        // insertar registros
+        MongoClient.connect(url,function(err,db){
+            console.log('conectando a db');
+            var collection = db.collection('productos');
+            collection.insertMany(jProductos,function(err,result){
+                console.log('datos guardados!');
+                db.close();
+            });
+        });
+    });
+}
+
+http.request(options, callback).end();
